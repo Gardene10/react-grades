9d4b95c35617a333a0f6c701774ab06b47312db9
unknown <gardene.v.c@gmail.com>
2023-06-22 19:32:30 -0300

    some files updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const Page = () => {
   
   return (
     <div className="container mx-auto " >
-      <h1 className="text-5xl mb-5"> Estudantes</h1>
+      <h1 className="text-5xl mb-5"> Lista de Estudantes </h1>
       <StudentTable students={students} />
       
     </div>
